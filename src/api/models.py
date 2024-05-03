af97595c1ce81dc1b2487039e0f2c2439936681f
PabloMoraleh <117026954+PabloMoraleh@users.noreply.github.com>
2024-05-03 11:07:38 +0000

    first commit

--- src/api/models.py
+++ src/api/models.py
@@ -2,18 +2,80 @@ from flask_sqlalchemy import SQLAlchemy
 
 db = SQLAlchemy()
 
-class User(db.Model):
+class Usuario(db.Model):
+    __tablename__ = 'usuario'
+    # Here we define columns for the table person
+    # Notice that each column is also a normal Python instance attribute.
     id = db.Column(db.Integer, primary_key=True)
-    email = db.Column(db.String(120), unique=True, nullable=False)
-    password = db.Column(db.String(80), unique=False, nullable=False)
-    is_active = db.Column(db.Boolean(), unique=False, nullable=False)
+    nombre = db.Column(db.String(250), nullable=False)
+    apellido = db.Column(db.String(250), nullable=False)
+    email = db.Column(db.String(250), nullable=False)
+    articulos_favoritos = db.relationship('ArticulosFavoritos', backref='usuario', lazy=True)
+    cesta = db.relationship('Cesta', backref='usuario', lazy=True)
 
     def __repr__(self):
-        return f'<User {self.email}>'
-
+        return "<Usuario %r >" % self.nombre
+    
     def serialize(self):
         return {
             "id": self.id,
+            "nombre": self.nombre,
             "email": self.email,
-            # do not serialize the password, its a security breach
+            "apellido": self.apellido
+        }
+
+class Articulos(db.Model):
+    __tablename__ = 'articulos'
+    # Here we define columns for the table address.
+    # Notice that each column is also a normal Python instance attribute.
+    id = db.Column(db.Integer, primary_key=True)
+    nombre = db.Column(db.String(250))
+    precio = db.Column(db.Integer)
+    imagen = db.Column(db.String(250))
+    descripcion = db.Column(db.String(250), nullable=False)
+    articulos_favoritos = db.relationship('ArticulosFavoritos', backref='articulos', lazy=True)
+
+    def __repr__(self):
+        return "<Articulos %r >" % self.nombre
+    
+    def serialize(self):
+        return {
+            "id": self.id,
+            "nombre": self.nombre,
+            "precio": self.precio,
+            "imagen": self.imagen,
+            "descripcion": self.descripcion
+        }
+
+class ArticulosFavoritos(db.Model):
+    __tablename__ = 'articulosfavoritos'
+    id = db.Column(db.Integer, primary_key=True)
+    usuario_id = db.Column(db.Integer, db.ForeignKey("usuario.id"))
+    articulo_id = db.Column(db.Integer,db.ForeignKey("articulos.id"))
+
+    def __repr__(self):
+        return "<ArticulosFavoritos %r >" % self.id
+    
+    def serialize(self):
+        return {
+            "id": self.id,
+            "usuario_id": self.usuario_id,
+            "articulo_id": self.articulo_id
+        }
+    
+
+class Cesta(db.Model):
+    __tablename__ = 'cesta'
+    id = db.Column(db.Integer, primary_key=True)
+    usuario_id = db.Column(db.Integer, db.ForeignKey("usuario.id"))
+    articulo_id = db.Column(db.Integer,db.ForeignKey("articulos.id"))
+
+    def __repr__(self):
+        return "<Cesta %r >" % self.id
+    
+    def serialize(self):
+        return {
+            "id": self.id,
+            "usuario_id": self.usuario_id,
+            "articulo_id": self.articulo_id
         }
