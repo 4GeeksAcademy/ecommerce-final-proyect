0ec0a388d6cfdf63edf847aebd96a97923a4827b
PabloMoraleh <117026954+PabloMoraleh@users.noreply.github.com>
2024-05-08 17:02:01 +0000

    added createUser page and login modal to navbar

--- src/front/js/component/navbar.js
+++ src/front/js/component/navbar.js
@@ -3,11 +3,11 @@ import { Link } from "react-router-dom";
 import { useNavigate } from "react-router-dom"
 
 export const Navbar = () => {
-	const navigate = useNavigate()
-	return (
+    const navigate = useNavigate()
+    return (
         <nav className="navbar navbar-expand-lg navbar-light bg-light">
             <div className="container px-4 px-lg-5">
-                <a className="navbar-brand" href="./"><i class="fa-solid fa-shop"></i></a>
+                <a className="navbar-brand" href="./"><i className="fa-solid fa-shop"></i></a>
                 <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle Navigation">
                     <span className="navbar-toggler-icon"></span>
                 </button>
@@ -17,19 +17,59 @@ export const Navbar = () => {
                             <a className="nav-link active" aria-current="page" href="./">Inicio</a>
                         </li>
                         <li className="nav-item">
-						<a className="nav-link" href="./aboutUs" >Sobre nosotros</a>
-						</li>                        
+                            <a className="nav-link" href="./aboutUs" >Sobre nosotros</a>
+                        </li>
                     </ul>
-                    <form className="d-flex">
+                    <div className="d-flex">
                         <button className="btn btn-outline-dark me-2" type="submit">
                             <i className="fas fa-shopping-cart me-1"></i>
-                            <span className="badge bg-dark text-white ms-1 rounded-pill">0</span>                      
+                            <span className="badge bg-dark text-white ms-1 rounded-pill">0</span>
                         </button>
-						<button type="button" className="btn btn-outline-dark me-2">Login</button>
-						<button type="button" className="btn btn-primary">Sign-up</button>
-					</form>					                
+
+                        <button type="button" className="btn btn-primary me-2" data-bs-toggle="modal" data-bs-target="#exampleModal">
+                            Login
+                        </button>
+
+                        <div className="modal fade" id="exampleModal" tabIndex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+                            <div className="modal-dialog">
+                                <div className="modal-content">
+                                    <div className="modal-header mb-5">
+                                        <h1 className="modal-title fs-5" id="exampleModalLabel">New message</h1>
+                                        <button type="button" className="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                                    </div>
+                                    <div className="modal-body">
+                                        <form className="mx-1 mx-md-4">
+                                            <div className="d-flex flex-row align-items-center mb-4">
+                                                <i className="fas fa-envelope fa-lg me-3 fa-fw"></i>
+                                                <div data-mdb-input-init className="form-outline flex-fill mb-0">
+                                                    <input type="email" id="form3Example3c" className="form-control"
+                                                        name='email'  />
+                                                    <label className="form-label" htmlFor="form3Example3c">Your Email</label>
+                                                </div>
+                                            </div>
+
+                                            <div className="d-flex flex-row align-items-center mb-4">
+                                                <i className="fas fa-lock fa-lg me-3 fa-fw"></i>
+                                                <div data-mdb-input-init className="form-outline flex-fill mb-0">
+                                                    <input type="password" id="form3Example4c" className="form-control"
+                                                        name='password'  />
+                                                    <label className="form-label" htmlFor="form3Example4c">Password</label>
+                                                </div>
+                                            </div>
+                                        </form>
+                                    </div>
+                                    <div className="modal-footer">
+                                        <button type="button" className="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+                                        <button type="submit" className="btn btn-primary">Login</button>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+
+                        <Link to="/createUser" type="button" className="btn btn-primary">Sign-up</Link>
+                    </div>
                 </div>
             </div>
         </nav>
-		);
+    );
 };
--- src/front/js/pages/createUser.js
+++ src/front/js/pages/createUser.js
@@ -3,7 +3,7 @@ import React, { useState } from 'react';
 export const CreateAccount = () => {
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
-
+console.log(email);
   const handleSubmit = (event) => {
     event.preventDefault();
 
@@ -14,46 +14,79 @@ export const CreateAccount = () => {
       return;
     }
 
-    // CONSOLE LOG PARA VER SI TRAE LOS DATOS. ELIMINAR ANTES DE FINAL PORQUE SE VE LA CONTRASEÑA.
+    // CONSOLE LOG PARA VER SI TRAE LOS DATOS. ELIMINAR ANTES DE FINAL PORQUE SE VE LA CONTRASEÑA. Esto debe cambiar por un fetch con método POST
     console.log(`Creating account with email: ${email}, password: ${password}`);
   };
 
   return (
-    <div className="create-account-container">
-      <h2>Create Your Account</h2>
+    <section className="vh-100" style={{backgroundColor: "#eee"}}>
+  <div className="container h-100">
+    <div className="row d-flex justify-content-center align-items-center h-100">
+      <div className="col-lg-12 col-xl-11">
+        <div className="card text-black" style={{borderRadius: "25px"}}>
+          <div className="card-body p-md-5">
+            <div className="row justify-content-center">
+              <div className="col-md-10 col-lg-6 col-xl-5 order-2 order-lg-1">
 
-      <form onSubmit={handleSubmit}>
-        <div className="form-group">
-          <label htmlFor="email">Email Address</label>
-          <input
-            type="email"
-            id="email"
-            name="email"
-            value={email}
-            onChange={(event) => setEmail(event.target.value)}
-          />
+                <p className="text-center h1 fw-bold mb-5 mx-1 mx-md-4 mt-4">Sign up</p>
+
+                <form className="mx-1 mx-md-4" onSubmit={handleSubmit}>
+
+                  <div className="d-flex flex-row align-items-center mb-4">
+                    <i className="fas fa-envelope fa-lg me-3 fa-fw"></i>
+                    <div data-mdb-input-init className="form-outline flex-fill mb-0">
+                      <input type="email" id="form3Example3c" className="form-control"
+                      name='email' onChange={(event) => setEmail(event.target.value)} />
+                      <label className="form-label" htmlFor="form3Example3c">Your Email</label>
+                    </div>
+                  </div>
+
+                  <div className="d-flex flex-row align-items-center mb-4">
+                    <i className="fas fa-lock fa-lg me-3 fa-fw"></i>
+                    <div data-mdb-input-init className="form-outline flex-fill mb-0">
+                      <input type="password" id="form3Example4c" className="form-control" 
+                      name='password' onChange={(event) => setPassword(event.target.value)}/>
+                      <label className="form-label" htmlFor="form3Example4c">Password</label>
+                    </div>
+                  </div>
+
+                  <div className="d-flex flex-row align-items-center mb-4">
+                    <i className="fas fa-key fa-lg me-3 fa-fw"></i>
+                    <div data-mdb-input-init className="form-outline flex-fill mb-0">
+                      <input type="password" id="form3Example4cd" className="form-control" />
+                      <label className="form-label" htmlFor="form3Example4cd">Repeat your password</label>
+                    </div>
+                  </div>
+
+                  <div className="form-check d-flex justify-content-center mb-5">
+                    <input className="form-check-input me-2" type="checkbox" value="" id="form2Example3c" />
+                    <label className="form-check-label" htmlFor="form2Example3">
+                      I agree all statements in <a href="#!">Terms of service</a>
+                    </label>
+                  </div>
+
+                  <div className="d-flex justify-content-center mx-4 mb-3 mb-lg-4">
+                    <button type="submit" data-mdb-button-init data-mdb-ripple-init className="btn btn-primary btn-lg">Register</button>
+                  </div>
+
+                </form>
+
+              </div>
+              <div className="col-md-10 col-lg-6 col-xl-7 d-flex align-items-center order-1 order-lg-2">
+
+                <img src="https://mdbcdn.b-cdn.net/img/Photos/new-templates/bootstrap-registration/draw1.webp"
+                  className="img-fluid" alt="Sample image"/>
+
+              </div>
+            </div>
+          </div>
         </div>
-
-        <div className="form-group">
-          <label htmlFor="password">Password</label>
-          <input
-            type="password"
-            id="password"
-            name="password"
-            value={password}
-            onChange={(event) => setPassword(event.target.value)}
-          />
-        </div>
-
-        <button type="submit" className="create-account-button">
-          Create Account
-        </button>
-      </form>
-
-      <p className="login-link">
-        Already have an account? <a href="#">Log in here</a>
-      </p>
+      </div>
     </div>
+  </div>
+</section>
+
+
   );
 };
 
